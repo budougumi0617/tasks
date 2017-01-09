e4af6433a67c3cfc3020d1ef32078b35dbf40e0d
budougumi0617 <budougumi0617@gmail.com>
2017-01-09 23:32:03 +0900

    W.I.P. Implementing seek

--- seek.go
+++ seek.go
@@ -9,6 +9,14 @@ import (
 	"path"
 )
 
+// newSeek is instead of constructor.
+func newSeek(p []string) *seek {
+	if p == nil {
+		p = []string{"TODO", "FIXME", "UNDONE"}
+	}
+	return &seek{pattern: p, parse: parse}
+}
+
 func getCode(name string) io.Reader {
 	f, err := os.Open(name)
 	if err != nil {
@@ -31,12 +39,20 @@ func getType(name string) Language {
 	return l
 }
 
-func walkFunc(path string, info os.FileInfo, err error) error {
+type seek struct {
+	tasks   []*Task
+	pattern []string
+	parse   func(s string, r io.Reader, l Language, k []string) []*Task
+}
+
+// type is path/filepath/WalkFunc
+func (s *seek) walkFunc(path string, info os.FileInfo, err error) error {
 	if info.IsDir() {
 		return nil
 	}
-	fmt.Printf("Seek %s, %s\n", path, info.Name())
-	tasks := parse(info.Name(), getCode(path), getType(info.Name()), []string{"TODO"})
-	fmt.Println(tasks) //TODO
+	if l := getType(info.Name()); l != 0 {
+		s.tasks = append(s.tasks, s.parse(info.Name(), getCode(path), l, s.pattern)...)
+		fmt.Printf("result = %v\n", s.tasks)
+	}
 	return nil
 }
--- seek_test.go
+++ seek_test.go
@@ -1,12 +1,11 @@
-// Copyright © 2017 budougumi0617. All rights reserved.
-
-// Copyright © 2017 budougumi0617. All rights reserved.
-
 package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 )
 
@@ -17,20 +16,114 @@ const (
 func TestWalkFunc(t *testing.T) {
 	var tests = []struct {
 		root     string
-		expected []string
+		expected []*Task
 	}{
 		{
 			testdata,
-			[]string{""},
+			[]*Task{
+				&Task{FileName: "LcdWrapper.cs"},
+				&Task{FileName: "TestData.xaml"},
+			},
 		},
 	}
 
 	for _, test := range tests {
 		fmt.Println("Root is " + test.root)
-		filepath.Walk(test.root, walkFunc)
-		// got := parse(test.filename, strings.NewReader(test.input), test.lang, test.key)
-		// if !reflect.DeepEqual(got, test.expected) {
-		// 	t.Errorf("\nResult = %v\nExpected %v", got, test.expected)
-		// }
+		// Set stub
+		s := &seek{parse: func(filename string, r io.Reader, l Language, k []string) []*Task {
+			return []*Task{&Task{FileName: filename}}
+		}}
+		filepath.Walk(test.root, s.walkFunc)
+		if !reflect.DeepEqual(s.tasks, test.expected) {
+			t.Errorf("\nResult = %v\nExpected %v", s.tasks, test.expected)
+		}
+	}
+}
+
+func Test_newSeek(t *testing.T) {
+	tests := []struct {
+		input []string
+	}{
+		{[]string{"DUMMY", "DUMMY1"}},
+	}
+	for _, test := range tests {
+		got := newSeek(test.input)
+		want := &seek{pattern: test.input, parse: parse}
+		if !reflect.DeepEqual(got.pattern, want.pattern) {
+			t.Errorf("\nResult = %v\nExpected %v", got, want)
+		}
+	}
+}
+
+func Test_getCode(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want io.Reader
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getCode(tt.args.name); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getCode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getType(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Language
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getType(tt.args.name); got != tt.want {
+				t.Errorf("getType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_seek_walkFunc(t *testing.T) {
+	type fields struct {
+		tasks   []*Task
+		pattern []string
+		parse   func(s string, r io.Reader, l Language, k []string) []*Task
+	}
+	type args struct {
+		path string
+		info os.FileInfo
+		err  error
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &seek{
+				tasks:   tt.fields.tasks,
+				pattern: tt.fields.pattern,
+				parse:   tt.fields.parse,
+			}
+			if err := s.walkFunc(tt.args.path, tt.args.info, tt.args.err); (err != nil) != tt.wantErr {
+				t.Errorf("seek.walkFunc() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
