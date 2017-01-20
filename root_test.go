692c655aa8a331f8790c6b4a29dea55e493e27da
budougumi0617 <budougumi0617@gmail.com>
2017-01-20 19:11:10 +0900

    Add root.go and test.

---
+++ root_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestRoot_Projects(t *testing.T) {
+	type fields struct {
+		name string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []string
+	}{
+		{
+			"Get sub directory.",
+			fields{name: "tempdir"},
+			[]string{"test"},
+		},
+		{
+			"Get sub directories.",
+			fields{name: "tempdir"},
+			[]string{"test", "test2"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arange
+			rootDir, _ := ioutil.TempDir("", tt.fields.name)
+			r := &Root{
+				name: rootDir,
+			}
+
+			ioutil.TempFile(rootDir, "tempfile")
+			for _, subdir := range tt.want {
+				fpath := filepath.Join(rootDir, subdir)
+				os.Mkdir(fpath, 0644)
+			}
+
+			// Act
+			if got := r.Projects(); !reflect.DeepEqual(got, tt.want) {
+				// Assert
+				t.Errorf("Root.Projects() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
